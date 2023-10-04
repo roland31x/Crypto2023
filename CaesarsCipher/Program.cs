6a1d092e0869555c81e96b0ff7e49a2b8e74abdf
roland31x <roland31x@gmail.com>
2023-10-04 13:34:42 +0300

    add

--- CaesarsCipher/Program.cs
+++ CaesarsCipher/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Caesar_Cipher
+﻿
+using MyCryptography;
+
+namespace CaesarsCipher
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Cipher Caesars = new 
+            
         }
     }
 }
---
+++ MyCryptography/MyCryptography.cs
@@ -0,0 +1,9 @@
+﻿namespace MyCryptography
+{
+    public abstract class Cipher
+    {
+        public abstract string Encrypt(byte[] plainText);
+        public abstract string Decrypt(byte[] encryptedText);
+        public abstract string Analyze(byte[] text);
+    }
+}
